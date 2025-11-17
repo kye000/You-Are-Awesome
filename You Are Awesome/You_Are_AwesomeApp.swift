a8ddfe9e038c7e0780201e80f3389a076363a4df
Kyle S <ksumagang2026@gmail.com>
2025-11-17 14:53:37 -0800

    Initial Commit

---
+++ You Are Awesome/You_Are_AwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  You_Are_AwesomeApp.swift
+//  You Are Awesome
+//
+//  Created by SUMAGANG, KYLE RUSSLIE on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct You_Are_AwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
